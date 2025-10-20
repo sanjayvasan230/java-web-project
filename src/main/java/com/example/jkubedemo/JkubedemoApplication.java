5da20f7f05690548f5e84d754e56cf4e02c24c19
sanjayvasan230 <sanjaymsd230@gmail.com>
2025-10-20 12:58:51 +0530

    init commit

---
+++ src/main/java/com/example/jkubedemo/JkubedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jkubedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkubedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkubedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jkubedemo/JkubedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jkubedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkubedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
